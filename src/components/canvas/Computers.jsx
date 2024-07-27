41489c4a05aefc59e1ee3c846ea897f57ff4f665
P Aditya Rao <adiarprrr23@gmail.com>
2024-07-27 12:57:16 +0530

    "Refactor App, Hero, Loader, Computers, and Earth components: updated imports, removed Feedbacks component, and made various code formatting and syntax changes."

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,28 +1,45 @@
-import React, { Suspense, useEffect,useState } from "react";
+import React, { Suspense,useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
 
-const Computers = ({isMobile}) => {
+const Computers = ({ isMobile }) => {
   const { scene } = useGLTF("./desktop_pc/scene.gltf");
+
+  useEffect(() => {
+    scene.traverse((object) => {
+      if (object.isMesh) {
+        const geometry = object.geometry;
+        const position = geometry.attributes.position;
+
+        if (position) {
+          const array = position.array;
+          const hasNaN = array.some(isNaN);
+
+          if (hasNaN) {
+            console.error("Geometry contains NaN values:", object);
+          }
+        }
+      }
+    });
+  }, [scene]);
 
   return (
     <mesh>
-      <hemisphereLight intensity={2} groundColor="black" />
+      <hemisphereLight intensity={1} groundColor="black" />
       <spotLight
-        position={[-30, 70, 70]}
+        position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={4}
+        intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={4} />
+      <pointLight intensity={3} />
       <primitive
         object={scene}
-        scale={isMobile?0.7:0.75}
-        position={isMobile?[0,-3,-2.2]:[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -30,25 +47,26 @@ const Computers = ({isMobile}) => {
 };
 
 const ComputersCanvas = () => {
-  const [isMobile,setIsMobile]=useState(false)
+  const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(()=>{
-    const mediaQuery=window.matchMedia('(max-width:500px)')
-    setIsMobile(mediaQuery.matches)
-    const handleMediaQueryChange=(event)=>{
-      setIsMobile(event.matches)
-    }
-    mediaQuery.addEventListener('change',handleMediaQueryChange)
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
 
-    return
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
 
-  })
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
   return (
     <Canvas
       frameloop="demand"
-      shadows
-      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
@@ -58,9 +76,9 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile}/>
+        <Computers isMobile={isMobile} />
       </Suspense>
-      <Preload all/>
+      <Preload all />
     </Canvas>
   );
 };
